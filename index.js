375cf0e92a68244cca34512a81c7a82f99b23077
Stephane Demotte <stephanedemotte@iMac-de-Stephane.local>
2015-12-10 13:45:09 -0500

    fix

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ var gulp     = require('gulp');
 var _        = require('underscore');
 var Elixir   = require('laravel-elixir');
 var tinypng	 = require('gulp-tinypng-compress');
-var config   = Elixir.config;
 
 Elixir.extend('tinypng', function(options) {
 
-    config.images = _.extend({
+		options = _.extend({
 			src: config.assetsPath + '/img',
 			output: config.publicPath + '/img',
 			tinypngKey: ''
-    }, config.images || {});
+		}, options);
 
     new Elixir.Task('tinypng', function () {
-			return gulp.src(config.images.src + '/**/*.{png,jpg,jpeg}')
+			return gulp.src(options.src + '/**/*.{png,jpg,jpeg}')
 				.pipe(tinypng({
-						key: config.images.tinypngKey,
+						key: options.tinypngKey,
 						checkSigs: true,
-						sigFile: config.images.src + '/.tinypng-sigs',
+						sigFile: options.src + '/.tinypng-sigs',
 						log: true
 					}))
-				.pipe(gulp.dest(config.images.output))
+				.pipe(gulp.dest(options.output))
 				.pipe(new Elixir.Notification('Tinypng Complete!'))
     })
-    .watch(config.images.src + '/**/*.{png,jpg,jpeg}')
+    .watch(options.src + '/**/*.{png,jpg,jpeg}')
 });
+
